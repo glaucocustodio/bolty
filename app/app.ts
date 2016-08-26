54c44146ec1ed7b2a72385cfaf6ada891f6afa51
Glauco Custódio <glauco.custodio@gmail.com>
2016-08-26 17:22:47 -0300

    Set side menu. Create signup page.

--- app/app.ts
+++ app/app.ts
@@ -1,17 +1,26 @@
-import {Component} from '@angular/core';
-import {Platform, ionicBootstrap} from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {Platform, ionicBootstrap, MenuController, NavController} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
-import {TabsPage} from './pages/tabs/tabs';
+//import {TabsPage} from './pages/tabs/tabs';
+import {LoginPage} from './pages/login/login';
+import {SignupPage} from './pages/signup/signup';
 
 
 @Component({
-  template: '<ion-nav [root]="rootPage"></ion-nav>'
+  templateUrl: './build/app.html',
+  providers: [NavController]
 })
 export class MyApp {
-
+  @ViewChild('nav') nav: NavController;
   private rootPage: any;
+  private pages: any[];
 
-  constructor(private platform: Platform) {
+  constructor(private platform: Platform, private menu: MenuController) {
+    this.menu = menu;
+    this.pages = [
+        { title: 'Home', component: LoginPage },
+        { title: 'Signup', component: SignupPage },
+    ];
     this.rootPage = LoginPage;
 
     platform.ready().then(() => {
@@ -19,6 +28,15 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
     });
+  }
+
+  openPage(page) {
+    console.log('open PAge mmm')
+
+    this.menu.close()
+    // Using this.nav.setRoot() causes
+    // Tabs to not show!
+    this.nav.push(page.component);
   }
 }
 
--- app/pages/login/login.ts
+++ app/pages/login/login.ts
@@ -1,10 +1,19 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {SignupPage} from '../signup/signup';
 
 @Component({
-  templateUrl: 'build/pages/login/login.html'
+  templateUrl: 'build/pages/login/login.html',
 })
 export class LoginPage {
-  constructor() {
+  private signupPage: any;
 
+  constructor(public nav: NavController) {
+     this.signupPage = SignupPage
+     this.nav = nav
+  }
+
+  openPage(page) {
+    this.nav.push(page);
   }
 }
