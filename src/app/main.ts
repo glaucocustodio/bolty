4be5df93602ff5c09cfb27663159fe4f8b70a632
Glauco Custódio <glauco.custodio@gmail.com>
2017-03-02 11:09:46 -0300

    Enable prod mode

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
+import {enableProdMode} from '@angular/core';
+
+// this is the magic wand - must use: ionic build --prod
+enableProdMode();
 
 platformBrowserDynamic().bootstrapModule(AppModule);
